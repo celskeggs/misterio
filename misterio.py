38c4b76142a2a2993cf024974404d1a5d98135bd
Colby Skeggs <skeggsc@catlin.edu>
2015-03-24 12:29:57 -0700

    Some initial implementation for character selection.

--- misterio.py
+++ misterio.py
@@ -1,4 +1,4 @@
-import webapp2, os, cgi, datetime, sys, time, logging, json
+import webapp2, os, cgi, datetime, sys, time, logging, json, urllib
 from google.appengine.api import users
 from google.appengine.ext import ndb
 
@@ -7,18 +7,53 @@ class DataBlock(ndb.Model):
 
 class TestPage(webapp2.RequestHandler):
 	def get(self):
+		character = self.request.cookies.get('character', None)
+		if character == None:
+			return self.redirect("/select")
+		with open("static/index.html", "r") as f:
+			self.response.headers["Content-Type"] = "text/html"
+			data = []
+			while True:
+				datum = f.read(8192)
+				if not datum: break
+				data.append(datum)
+			self.response.write("".join(data))
+class SelectPage(webapp2.RequestHandler):
+	def get(self):
+		character = self.request.cookies.get('character', None)
+		if character != None:
+			return self.redirect("/")
+		cid = self.request.get("id", None)
+		if cid != None:
+			self.response.set_cookie("character", str(cid))
+			return self.redirect("/")
 		with open("static/select.html", "r") as f:
 			self.response.headers["Content-Type"] = "text/html"
+			data = []
 			while True:
-				data = f.read(8192)
-				if not data: break
-				self.response.write(data)
+				datum = f.read(8192)
+				if not datum: break
+				data.append(datum)
+			prefix, ifany, rep, nochars, postfix = ''.join(data).split("<? CHARACTERS ?>")
+			chars = [(17, "arturo_d_b_a.png", "Sr. Loquacious Verylongname", "2015 Misterio Group 1")]
+			prefix = prefix.replace("<? USERNAME ?>", cgi.escape(users.get_current_user().nickname()))
+			if chars:
+				data = prefix + ifany + "\n".join(rep.replace("<? CID ?>", str(cid)).replace("<? AVATAR ?>", urllib.quote_plus(avatar)).replace("<? NAME ?>", cgi.escape(name)).replace("<? SESSION ?>", cgi.escape(session)) for cid, avatar, name, session in chars) + postfix
+			else:
+				data = prefix + nochars + postfix
+			self.response.write(data)
+class LogoffPage(webapp2.RequestHandler):
+	def get(self):
+		self.response.delete_cookie("character")
+		self.redirect(users.create_logout_url('/'))
 class TestPage2(webapp2.RequestHandler):
 	def get(self):
 		self.response.headers["Content-Type"] = "text/plain"
 		self.response.write("HELLO WORLD #2\n")
 
 application = webapp2.WSGIApplication([
+	('/logoff', LogoffPage),
+	('/select', SelectPage),
 	('/[a-z]*', TestPage),
 	('/dynamic/', TestPage2),
 ])
