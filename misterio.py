bb2c4c12ff71c266e2dd8cc60f40f39014bd3741
Colby Skeggs <skeggsc@catlin.edu>
2015-03-24 15:54:03 -0700

    Some initial work on the admin interface.

--- misterio.py
+++ misterio.py
@@ -1,9 +1,30 @@
-import webapp2, os, cgi, datetime, sys, time, logging, json, urllib
+import webapp2, os, cgi, datetime, sys, time, logging, json, urllib, jinja2
 from google.appengine.api import users
 from google.appengine.ext import ndb
 
-class DataBlock(ndb.Model):
-	contents = ndb.BlobProperty(required=True)
+class Template(ndb.Model):
+	name = ndb.StringProperty(required=True)
+class MessageSet(ndb.Model):
+	tag = ndb.StringProperty(required=True)
+class Character(ndb.Model):
+	name = ndb.StringProperty(required=True)
+	avatar = ndb.StringProperty(required=True)
+class Message(ndb.Model):
+	msid = ndb.KeyProperty(required=True, kind=MessageSet)
+	cid = ndb.KeyProperty(required=True, kind=Character)
+	title = ndb.StringProperty(required=True)
+	body = ndb.TextProperty(required=True)
+
+class Session(ndb.Model):
+	name = ndb.StringProperty(required=True)
+	activated = ndb.KeyProperty(repeated=True, kind=MessageSet)
+class Assignment(ndb.Model):
+	cid = ndb.KeyProperty(required=True, kind=Character)
+	player = ndb.UserProperty(required=True)
+class Post(ndb.Model):
+	cid = ndb.KeyProperty(required=True, kind=Character)
+	target = ndb.KeyProperty(kind=Character)
+	msg = ndb.TextProperty(required=True, indexed=False)
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
@@ -50,7 +71,7 @@ class DynamicPage(webapp2.RequestHandler):
 	def get(self, dynamic_id):
 		self.response.headers["Content-Type"] = "application/json"
 		if dynamic_id == "users":
-			o = {"me": 17, "session": "2015 Misterio Group 1", "access": False, "users": [{"cid": 17, "avatar": "arturo_d_b_a.png", "name": "Sra. Databasa Fletched"}]}
+			o = {"me": 17, "session": "2015 Misterio Group 1", "access": False, "users": [{"cid": 17, "avatar": "arturo_d_b_a.png", "name": "Sra. Remota Fletched"}]}
 		else:
 			return self.abort(404)
 		self.response.write(json.dumps(o))
@@ -60,4 +81,30 @@ application = webapp2.WSGIApplication([
 	('/select', SelectPage),
 	('/[a-z]*', MainPage),
 	('/dynamic/([a-z]+)', DynamicPage),
+])
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), "static_admin")),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class AdminPage(webapp2.RequestHandler):
+	def get(self, rel=""):
+		if rel == "":
+			self.response.headers["Content-Type"] = "text/html"
+			with open("static/admin/root.html", "r") as f:
+				data = []
+				while True:
+					datum = f.read(8192)
+					if not datum: break
+					data.append(datum)
+				data = "".join(data)
+				self.response.write(data)
+		else:
+			self.response.headers["Content-Type"] = "text/plain"
+			self.response.write("ADMIN WOAH: %s" % rel)
+
+administration = webapp2.WSGIApplication([
+	('/administration', AdminPage),
+	('/administration/(.*)', AdminPage),
 ])
