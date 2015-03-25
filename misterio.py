6d878393912da1170cd443522c18806dccfb8971
Colby Skeggs <skeggsc@catlin.edu>
2015-03-24 18:42:16 -0700

    Expanded admin interface a bunch.

--- misterio.py
+++ misterio.py
@@ -4,20 +4,19 @@ from google.appengine.ext import ndb
 
 class Template(ndb.Model):
 	name = ndb.StringProperty(required=True)
-class MessageSet(ndb.Model):
-	tag = ndb.StringProperty(required=True)
+	message_sets = ndb.StringProperty(repeated=True)
 class Character(ndb.Model):
 	name = ndb.StringProperty(required=True)
 	avatar = ndb.StringProperty(required=True)
 class Message(ndb.Model):
-	msid = ndb.KeyProperty(required=True, kind=MessageSet)
-	cid = ndb.KeyProperty(required=True, kind=Character)
-	title = ndb.StringProperty(required=True)
-	body = ndb.TextProperty(required=True)
+	msid = ndb.StringProperty(indexed=True, required=True)
+	cid = ndb.KeyProperty(indexed=True, required=False, kind=Character)
+	title = ndb.StringProperty(indexed=False, required=True)
+	body = ndb.TextProperty(indexed=False, required=True)
 
 class Session(ndb.Model):
 	name = ndb.StringProperty(required=True)
-	activated = ndb.KeyProperty(repeated=True, kind=MessageSet)
+	activated = ndb.StringProperty(repeated=True)
 class Assignment(ndb.Model):
 	cid = ndb.KeyProperty(required=True, kind=Character)
 	player = ndb.UserProperty(required=True)
@@ -87,24 +86,91 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), "static_admin")),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+JINJA_ENVIRONMENT.globals["len"] = len
+
+def safe_unwrap_key(expected_path, unsafe):
+	unsafe_key = ndb.Key(urlsafe=unsafe)
+	if len(unsafe_key.pairs()) != len(expected_path):
+		return None
+	for kind_id, expected in zip(unsafe_key.pairs(), expected_path):
+		if kind_id[0] != expected:
+			return None
+	return unsafe_key
 
 class AdminPage(webapp2.RequestHandler):
+	def safe_get_key(self, expected, field="key"):
+		rawkey = self.request.get(field, "")
+		if rawkey == "":
+			self.display_error("Missing request parameter: key")
+			return None
+		unwrapped = safe_unwrap_key(expected.split("/"), rawkey)
+		if unwrapped == None:
+			self.display_error("Invalid key.")
+			return None
+		return unwrapped
+	def display_error(self, error):
+		self.response.headers["Content-Type"] = "text/html"
+		jt = JINJA_ENVIRONMENT.get_template('error.html')
+		self.response.write(jt.render({"error": error}))
+	def post(self, rel=""):
+		if rel == "new_template":
+			name = self.request.get("name", "")
+			if name == "":
+				return self.display_error("Missing request parameter: name")
+			key = Template(name=name).put()
+			self.redirect("/administration/template?key=%s" % key.urlsafe())
+		elif rel == "add_message_set":
+			name = self.request.get("name", "")
+			if name == "":
+				return self.display_error("Missing request parameter: name")
+			key = self.safe_get_key("Template")
+			if key != None:
+				templ = key.get()
+				templ.message_sets.append(name)
+				templ.put()
+				self.redirect("/administration/template?key=%s" % key.urlsafe())
+		elif rel == "delete_message_set":
+			name = self.request.get("name", "")
+			if name == "":
+				return self.display_error("Missing request parameter: name")
+			key = self.safe_get_key("Template")
+			if key != None:
+				templ = key.get()
+				if name in templ.message_sets:
+					templ.message_sets.remove(name)
+					templ.put()
+					self.redirect("/administration/template?key=%s" % key.urlsafe())
+				else:
+					self.display_error("The specified message set was not found.")
+		else:
+			self.response.headers["Content-Type"] = "text/plain"
+			self.response.write("ADMIN: %s" % rel)
 	def get(self, rel=""):
 		if rel == "":
 			self.response.headers["Content-Type"] = "text/html"
-			with open("static/admin/root.html", "r") as f:
-				data = []
-				while True:
-					datum = f.read(8192)
-					if not datum: break
-					data.append(datum)
-				data = "".join(data)
-				self.response.write(data)
+			jt = JINJA_ENVIRONMENT.get_template('root.html')
+
+			templates = Template.query().fetch()
+
+			self.response.write(jt.render({"templates": templates}))
+		elif rel == "template":
+			self.response.headers["Content-Type"] = "text/html"
+			jt = JINJA_ENVIRONMENT.get_template('template.html')
+
+			key = self.safe_get_key("Template")
+			if key != None:
+				template = key.get()
+				global_messages = Message.query(Message.cid == None, ancestor=key).fetch()
+				if template == None:
+					self.display_error("The template that you are trying to view either does not exist or has been deleted.")
+				else:
+					self.response.write(jt.render({"template": template, "global_messages": global_messages}))
 		else:
 			self.response.headers["Content-Type"] = "text/plain"
-			self.response.write("ADMIN WOAH: %s" % rel)
+			self.response.write("ADMIN: %s" % rel)
 
 administration = webapp2.WSGIApplication([
 	('/administration', AdminPage),
 	('/administration/(.*)', AdminPage),
 ])
+
